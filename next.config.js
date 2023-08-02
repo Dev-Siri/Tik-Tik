dc840bce7b87e7eb460aac5f786d824f560968b7
Dev-Siri <s0431668@gmail.com>
2023-08-02 21:25:38 +0530

    Remove unneccessary config opts

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ["yt3.ggpht.com", "lh3.googleusercontent.com"],
   },
